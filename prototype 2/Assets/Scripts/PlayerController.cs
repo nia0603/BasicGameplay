b56220893d1ccf30379764c04aa1f339c3af124f
nia0603 <suarezn@roverkids.org>
2023-12-14 13:07:10 -0500

    2.1 & 2.2

--- prototype 2/Assets/Scripts/PlayerController.cs
+++ prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10.0f;
+    
+    
+    
 
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -14,11 +14,22 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.x < -xRange)
+       
+     transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        
+       if (transform.position.x > xRange)
+       
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z); 
+
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
